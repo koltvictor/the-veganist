c277d31c34fd7ea3378357e3455b5477d6009d5c
koltvictor <kolts.email@gmail.com>
2022-02-09 16:03:36 -0800

    configuring home page

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,5 +1,18 @@
-export default function Display() {
+export default function Display({recipes}) {
+    
+    const last = recipes[recipes.length - 1];
+    const lastThree = recipes.slice(Math.max(recipes.length -5, 1))
+    console.log(last)
+    console.log(lastThree)
+
+
     return(
-        <div></div>
+        <div>
+            <h2>Featured Recipes</h2>
+            <div className="recipeCard">
+                <img className="recipeImage" src={last.image} alt={last.name} />
+                <h3>{last.name}</h3>
+            </div>
+        </div>
     )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,3 +1,5 @@
+import Display from "./Display"
+
 export default function Home ({recipes}) {
 
     
@@ -6,6 +8,12 @@ export default function Home ({recipes}) {
             <h1>
                 Welcome to The Veganist
             </h1>
+            <h3>A Collection of Vegan Recipes</h3>
+            <p>Feel free to peruse and search the Recipes Index, see dishes by course, or add a recipe of your own!</p>
+            <br/>
+            <Display
+                recipes={recipes}
+            />
         </div>
     )
 }
